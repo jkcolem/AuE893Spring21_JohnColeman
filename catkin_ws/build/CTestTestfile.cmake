696f2df6194f663f562dbd269be4064a6305023d
jkcolem <jkcolem@g.clemson.edu>
2021-01-25 23:36:36 -0500

    All files but .xml file

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jkcolem/AuE893_JohnColeman_Sp21/git_ws/AuE893Spring21_JohnColeman/catkin_ws/src
+# Build directory: /home/jkcolem/AuE893_JohnColeman_Sp21/git_ws/AuE893Spring21_JohnColeman/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
